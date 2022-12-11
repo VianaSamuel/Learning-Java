4e5fbfccad30afac9f8d9cca952cabc09f8e3516
VianaSamuel <samuluizzz@hotmail.com>
2022-12-11 12:01:38 -0300

    Formatação.

--- A02EstruturasDeDados/Flexivel/Lista.java
+++ A02EstruturasDeDados/Flexivel/Lista.java
@@ -4,7 +4,7 @@ public class Lista {
     // o first sera SEMPRE um NODE CABECA de valor IRRELEVANTE //
     // o PRIMEIRO ELEMENTO sera SEMPRE first.getNext() //
 
-    private Node first, last;      // nodes -> primeiro e ultimo
+    private Node first, last;       // nodes -> primeiro e ultimo
     private int size;               // tamanho da estrutura
 
     public Lista(){
@@ -33,11 +33,12 @@ public class Lista {
         }
     //=====GETTERS & SETTERS=====//
 
-    //=====METODOS=====//
+    //=====ADD=====//
+        //-----ADD-----//
         public void add(int x, int pos){
             // se a posicao for 0, convoca o metodo de adicionar no INICIO
             if (pos == 0) addStart(x);
-            // se a posicao for (tamanho + 1), convoca o metodo de adicionar no FIM
+            // senao, se a posicao for igual ao TAMANHO DA ESTRUTURA, convoca o metodo de adicionar no FIM
             else if (pos == size) addEnd(x);
 
             // para outras posicoes
@@ -45,14 +46,15 @@ public class Lista {
                 // inicializa o percurso
                 Node previous = first;
 
-                // percorre a estrutura ate chegar ao ANTERIOR do NODE DESEJADO
+                // percorre ate o NODE ANTERIOR da POSICAO DESEJADA
                 for (int i = 0; i < pos; i++) previous = previous.getNext();
 
-                // cria node com o valor do input
+                // cria o novo node com o valor do input
                 Node temp = new Node(x);
 
-                // 
+                // "costura" DIREITA do NOVO NODE
                 temp.setNext(previous.getNext());
+                // "costura" DIREITA do NODE ANTERIOR
                 previous.setNext(temp);
 
                 size++;
@@ -61,135 +63,136 @@ public class Lista {
             }
         }
 
+        //-----ADD START-----//
         public void addStart(int x){
-            // cria node com o valor do input
+            // cria o novo node com o valor do input
             Node temp = new Node(x);
 
-            // atualiza o PROXIMO DO NOVO NODE como o ANTIGO PRIMEIRO NODE
+            // "costura" DIREITA do NOVO NODE com o ANTIGO PRIMEIRO NODE
             temp.setNext(first.getNext());
             // atualiza o PRIMEIRO NODE como o NOVO NODE
             first.setNext(temp);
 
-            // se a estrutura estiver vazia, atualiza tambem o ULTIMO NODE como o NOVO NODE
-            if (isEmpty()){
+            // se a estrutura estiver VAZIA (node cabeca == ultimo), atualiza tambem o ULTIMO NODE como o NOVO NODE
+            if (first == last){
                 last = temp;
             }
 
-            // incrementa o tamanho, libera o node temporario
             size++;
             temp = null;
         }
-    
+
+        //-----ADD END-----//
         public void addEnd(int x){
-            // cria node com o valor do input
+            // cria o novo node com o valor do input
             Node temp = new Node(x);
 
-            // atualiza o PROXIMO DO ULTIMO NODE como o NOVO NODE
+            // atualiza a DIREITA do ULTIMO NODE como o NOVO NODE
             last.setNext(temp);
             // atualiza o ULTIMO NODE como o NOVO NODE
             last = temp;
 
-            // incrementa o tamanho
             size++;
         }
-
+    //=====ADD=====//
+    //=====REMOVE=====//
+        //-----REMOVE-----//
         public int remove(int pos){
             // inicializa o retorno
             int removed = 0;
-
+        
+            // se a posicao for 0, convoca o metodo de remover o INICIO
             if (pos == 0) removed = removeStart();
+            // senao, se a posicao for igual ao TAMANHO DA ESTRUTURA, convoca o metodo de adicionar no FIM
             else if (pos == size-1) removed = removeEnd();
+
+            // para outras posicoes
             else {
                 // inicializa o percurso
                 Node previous = first;
-
-                // percorre ate o node ANTERIOR ao BUSCADO
+            
+                // percorre ate o NODE ANTERIOR da POSICAO DESEJADA
                 for (int i = 0; i < pos; i++) previous = previous.getNext();
-
-                // aponta o PROXIMO NODE do ANTERIOR como o BUSCADO e guarda o seu valor
+            
+                // aponta pro node desejado (DIREITA do ANTERIOR) e salva o seu valor
                 Node searched = previous.getNext();
                 removed = searched.getValue();
-
-                // REAPONTA o espaco a ser removido como o PROXIMO do BUSCADO
+            
+                // "costura" o ESPACO a ser REMOVIDO
                 previous.setNext(searched.getNext());
-
-                // APAGA o 
+            
+                // DESVINCULA o node desejado (remove a direita)
                 searched.setNext(null);
                 previous = null;
                 searched = null;
             }
-
+        
             size--;
             return removed;
         }
 
-        public int removeEnd(){
-            // inicializa o node temporario
-            Node temp;
-
-            // percorre a estrutura ate chegar ao ANTERIOR DO ULTIMO NODE
-            for (temp = first; temp.getNext() != last; temp = temp.getNext());
-
-            // guarda o valor removido
-            int removed = last.getValue();
-
-            // aponta o PENULTIMO NODE como o (NOVO) ULTIMO NODE
-            last = temp;
-
-            // apaga o ULTIMO NODE
-            last.setNext(null);
-
-            // libera o node temporario e retorna o valor apagado
-            temp = null;
-            size--;
-            return removed;
-        }
-    
+        //-----REMOVE START-----//
         public int removeStart(){
             // inicializa o NODE TEMPORARIO como o NODE CABECA  
             Node temp = first;
 
-            // aponta o PRIMEIRO NODE e GUARDA O SEU VALOR
+            // aponta pro node desejado (PRIMEIRO) e salva o seu valor
             first = first.getNext();
             int removed = first.getValue();
 
-            // libera o PRIMEIRO NODE e o temporario
+            // DESVINCULA o primeiro node
             temp.setNext(null);
 
-            // libera o node temporario e retorna o valor apagado
-            temp = null;
             size--;
+            temp = null;
             return removed;
         }
-    
+
+        //-----REMOVE END-----//
+        public int removeEnd(){
+            // inicializa o node temporario
+            Node i;
+
+            // percorre ate o NODE ANTERIOR ao ULTIMO NODE
+            for (i = first; i.getNext() != last; i = i.getNext());
+            // guarda o valor a ser removido
+            int removed = last.getValue();
+
+            // aponta o PENULTIMO NODE como o (NOVO) ULTIMO NODE
+            last = i;
+            // apaga o ULTIMO NODE
+            last.setNext(null);
+
+            size--;
+            i = null;
+            return removed;
+        }
+    //=====REMOVE=====//
+    //=====GET=====//
         public Node get(int pos){
             // inicializa o percurso
-            Node current = first.getNext();
+            Node i = first.getNext();
 
-            // enquanto o proximo node nao estiver vazio, percorre
-            for (int i = 0; i < pos; i++){
-                if (current.getNext() != null){
-                    current = current.getNext();
-                }
+            // caminha ate a posicao desejada se o proximo node nao estiver vazio
+            for (int k = 0; k < pos; k++){
+                if (i.getNext() != null) i = i.getNext();
             }
 
             // retorna o ultimo node
-            return current;
+            return i;
         }
-
+    //=====GET=====//
+    //=====SHOW=====//
         public void show(){
             System.out.print("[ ");
+
+            // caminha enquanto nao achar um ponteiro nulo
             for (Node i = first.getNext(); i != null; i = i.getNext()){
                 System.out.print(i.getValue() + " ");
             }
+
             System.out.print("]");
             System.out.println();
         }
-
-        public boolean isEmpty(){
-            // se o NODE CABECA for igual o ULTIMO NODE (nulo/vazio), entao a estrutura esta VAZIA
-            if (first == last) return true;
-            else return false;
-        }
-    //=====METODOS=====//
+    //=====SHOW=====//
 }
